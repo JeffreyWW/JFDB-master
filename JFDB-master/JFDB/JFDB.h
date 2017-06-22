1d1d723577482c00036d9a73f08b32368beba691
Jeffrey <jeffreyweiok@163.com>
2017-06-22 16:10:24 +0800

    no message

--- JFDB-master/JFDB/JFDB.h
+++ JFDB-master/JFDB/JFDB.h
@@ -4,7 +4,8 @@
 //
 //  Created by Jeffrey on 16/8/21.
 //  Copyright (c) 2016 Jeffrey. All rights reserved.
-//
+
+
 /**
  * 目的:旨在简化数据库操作,基于FMDB二次封装
  * JFTable类:把sql里的表形象话为对象进行操作,直接用OC进行对话操作,减少了sql语句的输入
